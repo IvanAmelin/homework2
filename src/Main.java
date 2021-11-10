d68e1bd2043d747a6a34311857262de2e12c8612
Ivan Amelin <ivmelinkvt@mail.ru>
2021-11-11 02:08:45 +0300

    2com

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.nio.file.FileSystemNotFoundException;
 public class Main {
     public static void main(String[] args) {
 
-        double ticket_price = 1019.99;
+        double ticketPrice = 1019.99;
         int bonus = 20;
-        double bonusmiles = ticket_price / bonus;
-        int bonusmilesint = (int)bonusmiles;
-        System.out.println("Бонусные мили: "+bonusmilesint);
+        double bonusMiles = ticketPrice / bonus;
+        int bonusMilesint = (int)bonusMiles;
+        System.out.println("Бонусные мили: "+bonusMilesint);
         // стоимости билета, другую для хранения количества
         // рублей для одной бонусной милли
 
